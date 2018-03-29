50e7600e49f29a755e60c71c6454e38263a64615
GODIN GUILLAUME <guillaume.godin94@gmail.com>
2018-03-29 09:11:19 +0200

    Premier commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# test-examen-android-CNAM-2-ggodin672
---
+++ app/src/main/java/com/example/ggodin672/androidcnam_2examentest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ggodin672.androidcnam_2examentest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
